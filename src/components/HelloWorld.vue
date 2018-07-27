9e6b7bb358c698c142fee0bbe5675d069a45429d
mona.song <3128019009@qq.com>
2018-07-27 16:32:57 +0800

    add_vue_demo_init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 @click="stepTo">Essential Links</h2>
+
+
+     <h2>{{info}}</h2>
+    <small-widget @setData="onSetData" :infoList="infoList"></small-widget>
+
+  </div>
+</template>
+
+<script>
+import SmallWidget from './SmallWidget.vue'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      info: '这是子组件设置之前的信息',
+      infoList: [
+        {name: 'lucy', age: '25'},
+        {name: 'lily', age: '23'},
+        {name: 'mona', age: '27'}
+      ]
+    }
+  },
+  components: {
+    SmallWidget
+  },
+  methods: {
+    onSetData (value) {
+      this.info = value;
+    },
+    stepTo () {
+      this.$router.push('/Main')
+    },
+  },
+  beforecreated () {
+    let curData = this.msg;
+    console.log('beforecreated', curData);
+  },
+  created () {
+    let curData = this.msg;
+    console.log('created', curData);
+  },
+  beforeMount () {
+    let curData = this.msg;
+    console.log('beforeMount', curData);
+  },
+  mounted () {
+    let curData = this.msg;
+    console.log('mounted', curData);
+  },
+  beforeDestroy () {
+    let curData = this.msg;
+    console.log('beforeDestroy', curData);
+  },
+  destroyed () {
+    let curData = this.msg;
+    console.log('destroyed', curData);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/SmallWidget.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="(item, $index) in infoList" :key="$index">
+      <p>{{item.name}}-{{item.age}}</p>
+    </li>
+  </ul>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  props: ['setData', 'infoList'],
+  created () {
+    setTimeout(()=>{
+      this.$emit('setData', '这是子组件设置之后的信息');
+    }, 2000)
+  },
+  mounted () {
+
+  }
+}
+</script>
+
